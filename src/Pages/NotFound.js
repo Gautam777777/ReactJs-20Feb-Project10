aafd277a620c68db91a08c949c38587485874a57
gautam777777 <gautamsharma0057@gmail.com>
2023-03-04 15:16:53 +0530

    pro10

---
+++ src/Pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+        <>
+            <div>NotFound</div>
+            <a href='/'>Go To Home Page</a>
+        </>
+  )
+}
